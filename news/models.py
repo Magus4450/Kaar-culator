e44b467548e23dde1608521fa514b1e96cf0b61a
Sugam Karki <sugamkarki4450@gmail.com>
2022-03-26 19:26:36 +0545

    Added ability to add, remove, delete, edit news

--- news/models.py
+++ news/models.py
@@ -1,10 +1,22 @@
 
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from datetime import datetime, date
 
 # Create your models here.
 
 User = get_user_model()
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('news:list')
+
 class NewsModel(models.Model):
 
     title = models.CharField(max_length=200)
@@ -12,6 +24,12 @@ class NewsModel(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
+    post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=100, default='General')
+
     def __str__(self):
         return self.title + '-' + str(self.author)
+
+    def get_absolute_url(self):
+        return reverse('news:detail', args=(str(self.id)))
 
--- news/views.py
+++ news/views.py
@@ -1,10 +1,58 @@
+
+
 from django.shortcuts import render
-from .models import NewsModel
-# ListView -> All news
-# DetailView -> One news in detail
-from django.views.generic import ListView, DetailView
+from .models import NewsModel, Category
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from .forms import PostForm, UpdateForm
 
-class HomeView(ListView):
+class NewsListView(ListView):
     model = NewsModel
-    template_name = 'templates/home.html'
+    template_name = 'templates/news/news.html'
+    context_object_name = 'news_list'
+
+    def get_context_data(self, *args, **kwargs):
+
+        cat_list = Category.objects.all()
+        context = super(NewsListView, self).get_context_data(*args, **kwargs)
+        context["cat_list"] = cat_list
+        return context
+
+
+def CategoryView(request, cats):
+    category_posts = NewsModel.objects.filter(category=cats.replace("-", ""))
+    return render(request, 'templates/news/categories.html', {'cats': cats.title().replace("-", " "), 'category_posts': category_posts})
+
+class NewsDetailView(DetailView):
+    context_object_name = 'news'
+    model = NewsModel
+    template_name = 'templates/news/news_detail.html'
+
+    
+
+class AddNewsView(CreateView):
+    model = NewsModel
+    form_class = PostForm
+    template_name = 'templates/news/add_news.html'
+
+
+class AddCategoryView(CreateView):
+    model = Category
+    template_name = 'templates/news/add_category.html'
+    fields = '__all__'
+
+    
+    
+
+class UpdateNewsView(UpdateView):
+    model = NewsModel
+    form_class = UpdateForm
+    template_name = 'templates/news/update_news.html'
+
+class DeleteNewsView(DeleteView):
+    context_object_name = 'news'
+    model = NewsModel
+    template_name = 'templates/news/delete_news.html'
+    success_url = reverse_lazy("news:list")
+    
